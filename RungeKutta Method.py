0a8798014dab5e64b26608594584a0d18a41ddc9
colindbailey <82052620+colindbailey@users.noreply.github.com>
2023-09-21 08:32:12 -0400

    Update RungeKutta Method.py

--- RungeKutta Method.py
+++ RungeKutta Method.py
@@ -13,17 +13,18 @@ def RungeKutta(f,x0,t):
     x[0] = x0
     for n in range (0,len(t)-1):
         e1 = h*f(x[n],t[n]) #defines all of the parts of the runge kutta
-        e2 = h*f(x[n]+(e1/2),t[n])
-        e3 = h*f(x[n]+(e2/2),t[n])
-        e4 = h*f(x[n]+e3,t[n])
+        e2 = h*f(x[n]+ 0.5*h,t[n] + 0.5*e1)
+        e3 = h*f(x[n]+ 0.5*h,t[n] + 0.5*e2)
+        e4 = h*f(x[n] + h,t[n] + e3)
         x[n+1] = x[n] + ((e1 +(2*e2)+(2*e3)+e4)/6) #puts them all together
     return x
 
 t0 = 0 #intial time point
-tf = 1 #final time point
-n = 0 #used to change the time step
+tf = 5 #final time point
+n = 0.001 #used to change the time step
 h = 10 ** (-n) # this creates the time step
 print ('Time Step = ', h)
+
 
 t = np.arange(t0,tf+h,h) #creates an array of the time step
 x0 = 1 #intial value
@@ -33,7 +34,7 @@ x = RungeKutta(f,x0,t) #runs the rungekutta function
 x_true = np.exp(-t) #has the true value of the function that the runge kutta will be compared to
 
 E = abs(x - x_true) #error between them
-#print(E) #This would print the difference in the values of the functions
+print(E) #This would print the difference in the values of the functions
 
 Ex = np.log(E)
 
